348e516a4ce55681ec0c1e2f8598ce0f3ce14f69
kjg2711 <kimjonggyu2711@gmail.com>
2024-03-17 07:45:21 +0900

    1장 연습문제 24번

---
+++ 1장 연습문제 24번-1.py
@@ -0,0 +1,22 @@
+# 1장 연습문제 24번 
+# 콤마로 숫자를 한번에 받아 리스트에 저장
+import heapq
+
+inputnum = input("숫자들을 입력하세요 (콤마로 구분): ") # 콤마로 구분된 숫자를 입력받음
+num1 = [int(num) for num in inputnum.split(',')] # 입력받은 문자열을 콤마를 기준으로 나누어 숫자 리스트로 변환
+
+freq = []
+
+n = len(num1)
+h=[] # 힙을 위한 리스트 
+mylist=[] # 빈 리스트
+
+for i in range(n) :
+    heapq.heappush(h,num1[i]) # freq를 h에 삽입
+
+for f in range(n):
+    mylist.append(heapq.heappop(h))  # heappop을 통해 h가 mylist로 
+
+print("h 리스트",num1)
+print("mylist에 저장된 숫자를 정렬하여 출력",mylist )
+    

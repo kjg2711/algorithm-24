1aebcb2c69426d66518680bf2c049a7cf6f458b6
kjg2711 <66630433+kjg2711@users.noreply.github.com>
2024-03-19 15:32:59 +0900

    Update 1장 연습문제 24번-1.py

--- 1장 연습문제 24번-1.py
+++ 1장 연습문제 24번-1.py
@@ -1,21 +1,20 @@
 # 1장 연습문제 24번 
-# 콤마로 숫자를 한번에 받아 리스트에 저장
+
 import heapq
 
 inputnum = input("숫자들을 입력하세요 (콤마로 구분): ") # 콤마로 구분된 숫자를 입력받음
-num1 = [int(num) for num in inputnum.split(',')] # 입력받은 문자열을 콤마를 기준으로 나누어 숫자 리스트로 변환
+num1 = [int(num) for num in inputnum.split(',')] # 입력받은 숫자를 콤마 기준으로 나누어 여러 입력 값을 저장 
 
-freq = []
+n = len(num1) # 길이 
 
-n = len(num1)
 h=[] # 힙을 위한 리스트 
 mylist=[] # 빈 리스트
 
 for i in range(n) :
-    heapq.heappush(h,num1[i]) # freq를 h에 삽입
+    heapq.heappush(h,num1[i]) # num1을 heapqpush를 통해 h에 추가
 
 for f in range(n):
-    mylist.append(heapq.heappop(h))  # heappop을 통해 h가 mylist로 
+    mylist.append(heapq.heappop(h))  # heappop을 통해 h 반환 append를 통해 반환한 h를 mylsit로 추가 
 
 print("힙 리스트 출력",num1)
 print("mylsit 리스트 정렬되어 출력",mylist )

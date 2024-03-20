192d09733162f0b072cafdb2c31a8f782c46d06c
kjg2711 <66630433+kjg2711@users.noreply.github.com>
2024-03-20 11:14:41 +0900

    Delete 18번.py

--- 18번.py
+++
@@ -1,9 +0,0 @@
-mylist = []  # 빈 리스트
-
-num = int(input("노드의 개수: "))  # 리스트의 크기
-
-for i in range(num):
-    data = int(input(f"노드 #{i + 1} 데이터: ")) # f 문자열 리터럴 
-    mylist.append(data)  # 리스트에 저장
-
-print("리스트의 내용:", mylist)  # 리스트에 저장된 데이터 출력1

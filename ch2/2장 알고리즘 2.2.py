ba1687b836d50c5776f8465fb9cbd2b463748acb
kjg2711 <66630433+kjg2711@users.noreply.github.com>
2024-06-17 03:12:11 +0900

    Delete ch2 directory

--- ch2/2장 알고리즘 2.2.py
+++
@@ -1,9 +0,0 @@
-# 알고리즘 A / 알고리즘 2.2 
-# 자연수의 제곱 계산 (O(1))
-
-n = int(input("compute_square_A(n): "))
-
-def compute_square_A(n) :       # n의 제곱 계산 함수. 알고리즘 A 사용
-    return n*n                  # 기본연산 n*n
-
-print(compute_square_A(n))      # 함수를 호출하여 결과를 반환하고 그 결과를 출력
